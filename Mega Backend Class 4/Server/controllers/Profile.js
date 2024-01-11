502f9089397aa353edeb0108efc0b28e4a72ad6d
Debapriyo Das <debapriyo00@gmail.com>
2024-01-11 23:39:08 +0530

    To be continue Profile Controller .

---
+++ Mega Backend Class 4/Server/controllers/Profile.js
@@ -0,0 +1,10 @@
+const Profile = require("../models/profile")
+const User = require("../models/user")
+
+exports.updateProfile = async(req, res) =>{
+    try {
+        
+    } catch (err) {
+        
+    }
+}
